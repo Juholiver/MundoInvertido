1e5b64721f7fd5a9029542e0772790896a43f53d
Jose Mario <juholiver3@gmail.com>
2022-08-28 18:06:35 -0300

    adcionando o formulario com js e banco de dados firebase

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,20 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAZA-n_wyGjXIYtCUnHpZ-kV6GT2HIArNY",
+    authDomain: "projeto-mundoinvertido-751f9.firebaseapp.com",
+    projectId: "projeto-mundoinvertido-751f9",
+    storageBucket: "projeto-mundoinvertido-751f9.appspot.com",
+    messagingSenderId: "641814273934",
+    appId: "1:641814273934:web:4115da4ab519ab0ec5cedf"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
+  export default app
